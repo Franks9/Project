db05a3e1abf9e8246e1c93da77a984dfe48cc82c
Francisco_Moreno <Francisco_Moreno@LAPTOP-M5K6SCLE>
2020-11-27 17:19:59 -0600

    version 0.0.0

---
+++ Proyecto/src/main/java/NewClass.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class NewClass {
+    public static void main(String[]args){
+        JFrame f1=new JFrame("titulo :D");
+        f1.setSize(1400, 900);
+        f1.setVisible(true);
+        
+    }
+}
